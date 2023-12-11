696650c8887305dd5633268f92f024e376a23ea2
Yogue1997 <yousyogue@gmail.com>
2023-12-10 20:21:34 -0500

    Working on redux

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -2,38 +2,43 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { Signup, Welcome, ForgotPassword } from './app/screen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import ScreenImport from './utils/ScreenImport';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 
-const Stack =  createNativeStackNavigator()
+const Stack = createNativeStackNavigator()
 
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='welcome'>
-        <Stack.Screen 
-          name='Welcome'
-          component={Welcome}
-          options={{
-            headerShown: false
-          }}
-        />
-        <Stack.Screen 
-          name='Signup'
-          component={Signup}
-          options={{
-            headerShown: false
-          }}
-        />
-        <Stack.Screen 
-          name='ForgotPassword'
-          component={ForgotPassword}
-          options={{
-            headerShown: false
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='welcome'>
+          <Stack.Screen
+            name='Welcome'
+            component={Welcome}
+            options={{
+              headerShown: false
+            }}
+          />
+          <Stack.Screen
+            name='Signup'
+            component={Signup}
+            options={{
+              headerShown: false
+            }}
+          />
+          <Stack.Screen
+            name='ForgotPassword'
+            component={ForgotPassword}
+            options={{
+              headerShown: false
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
---
+++ mobile-app/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "../redux/counter/counterSlice";
+
+export default configureStore({
+  reducer: {
+    counter: counterSlice,
+  },
+});
